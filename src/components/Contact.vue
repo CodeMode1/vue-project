7236b4bcd372df75fc43f1f08489bd97e8602dd4
CodeMode1 <daphnedc88@gmail.com>
2024-05-13 16:57:49 -0400

    Add contact component Note: Vue seems to have syntax rule configured in tsconfig I guess to remove ; on save. Ok.. will need to get used to this coding style :D

---
+++ src/components/Contact.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+var rand = Math.random()
+console.log(rand)
+</script>
+
+<template>
+  <h1>Hello this is a contact component {{ rand }}</h1>
+</template>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,6 +1,12 @@
+<script setup lang="ts">
+import Contact from '../components/Contact.vue'
+</script>
+
 <template>
   <div class="contact">
     <h1>This is a contact page</h1>
+    <!-- embedded comp -->
+    <Contact/>
   </div>
 </template>
 
